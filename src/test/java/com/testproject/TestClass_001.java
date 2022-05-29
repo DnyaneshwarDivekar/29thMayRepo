481597ab797e07c907b957ebb0f0499833967d4b
DnyaneshwarDivekar <dnyaneshwardivekar@gmail.com>
2022-05-29 10:08:50 +0530

    This P_001 project 1st commit

---
+++ src/test/java/com/testproject/TestClass_001.java
@@ -0,0 +1,18 @@
+package com.testproject;
+
+public class TestClass_001 {
+
+	public static void main(String[] args) {
+
+		TestClass_001 myObj = new TestClass_001();
+		myObj.print();
+		
+		
+	}
+	
+	void print() {
+		
+		System.out.println("This is a print method calling");
+	}
+
+}
